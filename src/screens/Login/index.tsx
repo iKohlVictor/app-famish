0eef6bd78c9c77beacf49d3955392658dc4c0709
fernando <fernando00sjp@hotmail.com>
2022-12-14 21:07:46 -0300

    implement-alert

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { ButtonIcon } from "@components/ButtonIcon";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import logoImage from "@assets/logo.png";
@@ -8,17 +8,35 @@ import logoImage from "@assets/logo.png";
 import { Container, Subtitle, Logo, Content } from "./styles";
 import { FlatButton } from "@components/Flat-Button";
 import { useState } from "react";
+
 export function Login() {
+
+  const showError = ()=>{
+    Alert.alert(
+      "Error",
+      "Usuário ou senha inválido!",
+      [
+        {
+          text:"Tentar Novamente",
+          style:"cancel",
+        },
+      ],
+      {
+        cancelable:true
+      }
+    )
+  }
+
   const [email,setEmail] = useState('')
   const [senha,setSenha] = useState('')
 
   const navigation = useNavigation();
 
   function handleMenu(){
-    if(email=="admin"){
+    if(email=="Admin"){
       navigation.navigate('menu')
     }else{
-      navigation.navigate('newUser')
+      showError();
     }
     
   }
